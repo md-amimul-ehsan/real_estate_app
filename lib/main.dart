53251585269d3a5eeca1977475e32b8978ff345a
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2021-03-12 23:46:51 +0600

    Remove auth screens to auth folder directly instead of placing them under screens folder inside auth folder.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:real_estate_app/views/main/main_screen.dart';
-import 'package:real_estate_app/views/auth/screens/sign_up_screen.dart';
+import 'package:real_estate_app/views/auth/sign_up_screen.dart';
 
 void main() => runApp(RealEstateApp());
 
