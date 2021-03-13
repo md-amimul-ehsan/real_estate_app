64839cc318c06f07846a850bdd992e7224a635dc
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2021-03-14 04:26:54 +0600

    Refactor agents main screen code.

--- lib/views/agents/agents_main_screen.dart
+++ lib/views/agents/agents_main_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:real_estate_app/controllers/properties.dart';
 import 'package:real_estate_app/models/property.dart';
 import 'package:real_estate_app/views/agents/agents_list_item.dart';
-import 'package:real_estate_app/views/properties/properties_list_item.dart';
 
 class AgentsScreen extends StatefulWidget {
   @override
@@ -13,28 +12,59 @@ class _AgentsScreenState extends State<AgentsScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: [
-          Expanded(
-            child: FutureBuilder(
-              future: getAllProperties(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<List<Property>> snapshot) {
-                return snapshot.data != null
-                    ? ListView.builder(
+      child: FutureBuilder(
+        future: getAllProperties(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<Property>> snapshot) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // IconButton(
+                  //   icon: Icon(
+                  //     Icons.search,
+                  //     color: Colors.black,
+                  //   ),
+                  //   onPressed: () {
+                  //     showSearch(
+                  //         context: context,
+                  //         delegate: snapshot.data != null ? PropertiesSearchDelegate(propertyList: snapshot.data) : PropertiesSearchDelegate()
+                  //     );
+                  //   },
+                  //   iconSize: 30,
+                  // ),
+                  Text(
+                    "Agents",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                  height: 10
+              ),
+              snapshot.data != null // Replace with agents recycler
+                  ? Expanded(
+                    child: ListView.builder(
                         scrollDirection: Axis.vertical,
                         itemCount: snapshot.data.length,
-                        //Replace with actual number of properties in fetched data
                         itemBuilder: (context, i) {
                           return agentsListItem(
                             snapshot.data[i],
-                          ); //Pass index i of snapshot data (list of Property objects)
-                        })
-                    : Text("No property found"); //Replace with spinner
-              },
-            ),
-          ),
-        ],
+                          );
+                        },
+                      ),
+                  )
+                  : Text("No agent found"),
+            ],
+          );
+        },
       ),
     );
   }
