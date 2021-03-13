cec5465a43efa8ad8847be8961aabb1beb13c932
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2021-03-14 03:59:45 +0600

    Remove search functionality for now.

--- lib/views/properties/properties_main_screen.dart
+++ lib/views/properties/properties_main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:real_estate_app/controllers/properties.dart';
 import 'package:real_estate_app/models/property.dart';
+import 'package:real_estate_app/views/properties/properties_details.dart';
 import 'package:real_estate_app/views/properties/properties_recycler.dart';
 
 class PropertiesScreen extends StatefulWidget {
@@ -22,19 +23,19 @@ class _PropertiesScreenState extends State<PropertiesScreen> {
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.search,
-                      color: Colors.black,
-                    ),
-                    onPressed: () {
-                      showSearch(
-                          context: context,
-                          delegate: snapshot.data != null ? PropertiesSearchDelegate(propertyList: snapshot.data) : PropertiesSearchDelegate()
-                      );
-                    },
-                    iconSize: 30,
-                  ),
+                  // IconButton(
+                  //   icon: Icon(
+                  //     Icons.search,
+                  //     color: Colors.black,
+                  //   ),
+                  //   onPressed: () {
+                  //     showSearch(
+                  //         context: context,
+                  //         delegate: snapshot.data != null ? PropertiesSearchDelegate(propertyList: snapshot.data) : PropertiesSearchDelegate()
+                  //     );
+                  //   },
+                  //   iconSize: 30,
+                  // ),
                   Text("Properties",
                     textAlign: TextAlign.center,
                     style: TextStyle(
@@ -56,56 +57,67 @@ class _PropertiesScreenState extends State<PropertiesScreen> {
   }
 }
 
-class PropertiesSearchDelegate extends SearchDelegate<Property>
-{
-  PropertiesSearchDelegate({
-    this.propertyList,
-  });
-
-  final List<Property> propertyList;
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: (){
-          query = ""; //Clear query string
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-        icon: Icon(Icons.keyboard_arrow_left),
-        onPressed: (){
-          close(context, null);
-        },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    // Navigate to property details screen with result
-    return null;
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    final list = query.isEmpty ? propertyList : propertyList.where((element) => element.name.toLowerCase().startsWith(query.toLowerCase())).toList();
-    return ListView.builder(
-        itemCount: list.length,
-        itemBuilder: (context, i) {
-          return ListTile(
-            title: Text(list[i].name),
-            onTap: () {
-              showResults(context);
-              print("$i search item pressed");
-            },
-          );
-        },
-    );
-  }
-
-}
+// class PropertiesSearchDelegate extends SearchDelegate<Property>
+// {
+//   PropertiesSearchDelegate({
+//     this.propertyList,
+//   });
+//
+//   final List<Property> propertyList;
+//   Property property;
+//   @override
+//   List<Widget> buildActions(BuildContext context) {
+//     return [
+//       IconButton(
+//         icon: Icon(Icons.clear),
+//         onPressed: (){
+//           query = ""; //Clear query string
+//         },
+//       ),
+//     ];
+//   }
+//
+//   @override
+//   Widget buildLeading(BuildContext context) {
+//     return IconButton(
+//         icon: Icon(Icons.keyboard_arrow_left),
+//         onPressed: (){
+//           close(context, null);
+//         },
+//     );
+//   }
+//
+//   @override
+//   Widget buildResults(BuildContext context) {
+//     // close(context, property);
+//     Navigator.push(
+//       context,
+//       MaterialPageRoute(builder: (context) {
+//         return PropertiesDetails(property: property);
+//       }),
+//     );
+//   }
+//
+//   @override
+//   Widget buildSuggestions(BuildContext context) {
+//     final list = query.isEmpty ? propertyList : propertyList.where((element) => element.name.toLowerCase().startsWith(query.toLowerCase())).toList();
+//     return ListView.builder(
+//         itemCount: list.length,
+//         itemBuilder: (context, i) {
+//           return ListTile(
+//             title: Text(list[i].name),
+//             onTap: () {
+//               this.property = list[i];
+//               showResults(context);
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (context) {
+//                   return PropertiesDetails(property: list[i]);
+//                 }),
+//               );
+//             },
+//           );
+//         },
+//     );
+//   }
+// }
