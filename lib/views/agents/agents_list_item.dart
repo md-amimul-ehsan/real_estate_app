71d39d19088224df8650e0a999bf7c7df61218d2
Amimul Ehsan <50029846+nkshn22@users.noreply.github.com>
2021-03-16 20:05:46 +0600

    made the card look bigger and more beautiful

--- lib/views/agents/agents_list_item.dart
+++ lib/views/agents/agents_list_item.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:real_estate_app/models/property.dart';
-import 'package:real_estate_app/utilities/constants.dart';
 import 'package:real_estate_app/views/agents/agents_details.dart';
 
 class AgentsListItem extends StatefulWidget {
@@ -29,46 +28,57 @@ class _AgentsListItemState extends State<AgentsListItem> {
       },
       child: Card(
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(25),
+          borderRadius: BorderRadius.circular(15),
         ),
-        elevation: 5,
+        elevation: 8,
         margin: EdgeInsets.symmetric(
           horizontal: 10,
           vertical: 5,
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CircleAvatar(
-                radius: 30,
-                foregroundColor: Colors.black,
-                foregroundImage: NetworkImage(
-                  "https://giantbomb1.cbsistatic.com/uploads/scale_medium/1/16944/2427349-426065_10151435086863987_724057164_n.jpg",
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  radius: 30,
+                  foregroundColor: Colors.black,
+                  foregroundImage: NetworkImage(
+                    "https://giantbomb1.cbsistatic.com/uploads/scale_medium/1/16944/2427349-426065_10151435086863987_724057164_n.jpg",
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              width: 5,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Agent name",
-                ),
-                Text(
-                  "Agent description",
-                ),
-              ],
-            ),
-            Spacer(),
-            Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: Icon(Icons.arrow_forward_ios),
-            ),
-          ],
+              SizedBox(
+                width: 5,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Agent name",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
+                  Text(
+                    "Agent description",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Icon(Icons.arrow_forward_ios),
+              ),
+            ],
+          ),
         ),
       ),
     );
