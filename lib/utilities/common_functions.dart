35e55dcb9cb65e529b333c6539da72dae904ab69
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2021-03-17 19:15:58 +0600

    Add a few common sharedPref functions and a snackbar function.

--- lib/utilities/common_functions.dart
+++ lib/utilities/common_functions.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 String timestampToNeatTime(String timestamp, )
 {
@@ -8,4 +10,51 @@ String timestampToNeatTime(String timestamp, )
   final clockString = format.format(dateTime);
 
   return clockString;
+}
+
+Future<bool> checkIfUserLoggedIn() async {
+  try {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    if ( prefs.containsKey("UID") ) {
+      return true;
+    } else if ( prefs.getString("UID") == null ){
+      return false;
+    }
+  } catch (e) {
+    return false;
+  }
+}
+
+Future<void> setLoginDetails(String uid, String jwt) async {
+  try {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString("UID", uid);
+    await prefs.setString("JWT", jwt);
+  } catch (e) {
+    print("Error storing login credentials");
+  }
+}
+
+Future<void> removeLoginDetails() async {
+  try {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove("UID");
+    prefs.remove("JWT");
+  } catch (e) {
+    print("Error removing login credentials");
+  }
+}
+
+void showSnackbar(dynamic icon, String message, dynamic color, BuildContext context) {
+  SnackBar mySnackbar = SnackBar(
+      duration: Duration(seconds: 2),
+      content: Row(
+        children: [
+          Icon(icon),
+          SizedBox(width: 10),
+          Expanded(child: Text(message)),
+        ],
+      ),
+      backgroundColor: color);
+  ScaffoldMessenger.of(context).showSnackBar(mySnackbar);
 }
