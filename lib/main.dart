6fec9c40442079e87bab8782681af6346a7822c4
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2021-03-17 19:14:08 +0600

    Check to see if user is signed in or not before navigating to main screen or auth screens.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:real_estate_app/utilities/common_functions.dart';
 import 'package:real_estate_app/views/main/main_screen.dart';
 import 'package:real_estate_app/views/auth/landing_screen.dart';
 
-void main() => runApp(RealEstateApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  bool loginFlag = await checkIfUserLoggedIn();
+  runApp(RealEstateApp(loginFlag: loginFlag));
+}
 
-class RealEstateApp extends StatelessWidget {
-  RealEstateApp({Key key}) : super(key: key);
+class RealEstateApp extends StatefulWidget {
+  RealEstateApp({this.loginFlag, Key key}) : super(key: key);
 
   static const String _title = 'Flutter Code Sample';
-  final bool login = false;
+  bool loginFlag;
+  @override
+  _RealEstateAppState createState() => _RealEstateAppState();
+}
+
+class _RealEstateAppState extends State<RealEstateApp> {
+  bool login;
+
+  @override
+  Future<void> initState() {
+    // TODO: implement initState
+    super.initState();
+    login = widget.loginFlag;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      home: login ? LandingScreen() : MainScreen(),
+      title: RealEstateApp._title,
+      home: login ? MainScreen() : LandingScreen()
     );
   }
 }
